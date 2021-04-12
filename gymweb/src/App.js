2d6f724e2c2cb97e66950a1f6e94744da0d41480
LongTran0919 <hoanglong090320@gmail.com>
2021-04-12 19:29:01 +0700

    Giao diện đăng ký và thành navbar

--- gymweb/src/App.js
+++ gymweb/src/App.js
@@ -1,10 +1,22 @@
-
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Frontend/component/Navbar';
+import Header from './Frontend/component/Header';
+import Form from './Frontend/component/Form';
+import {BrowserRouter as Router,Switch,Route} from'react-router-dom';
+
+
 
 
 function App() {
   return (
-     <div>test</div>
+    <Router>
+        <Navbar/>
+        <Switch>
+            <Route path="/home" component={Header}/>
+            <Route path="/signup" component={Form}/>
+        </Switch>
+ </Router>
   );
 }
 
---
+++ gymweb/src/Frontend/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Header = () => {
+    return (
+      <div className="header-wraper">
+          <div className="main-info">
+          <h1>WebDeveloper</h1>
+          <a href="#" className="btn-main-offer">Contact me</a>
+          </div>
+      </div>
+    )
+}
+
+export default Header;
