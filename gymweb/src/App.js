8db11a21f7ef6f0a651ab43f9a86a4f04c1206cd
dajneem23 <56623206+dajneem23@users.noreply.github.com>
2021-04-14 00:58:13 +0700

    Revert "set card"
    This reverts commit 87acb3ddc3dcc76bd3afcb6a31d5c926490458b7.

--- gymweb/src/App.js
+++ gymweb/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './Frontend/component/Navbar';
+import Header from './Frontend/component/Header';
+<<<<<<< HEAD
+<<<<<<< HEAD
 import Register from './Frontend/component/formRegister/Register';
 import SignIn from './Frontend/component/formSignIn/SignIn'
+=======
+import Form from './Frontend/component/Form';
+>>>>>>> parent of 6b788a3 (Merge branch 'main' of https://github.com/LongTran0919/GymWeb into main)
+=======
+import Form from './Frontend/component/Form';
+>>>>>>> parent of 6b788a3 (Merge branch 'main' of https://github.com/LongTran0919/GymWeb into main)
 import {BrowserRouter as Router,Switch,Route} from'react-router-dom';
-import Home from './Frontend/component/formHome/Home';
 
 
 
@@ -14,9 +22,17 @@ function App() {
     <Router>
         <Navbar/>
         <Switch>
-            <Route path="/home" component={Home}/>
+            <Route path="/home" component={Header}/>
+<<<<<<< HEAD
+<<<<<<< HEAD
             <Route path="/signup" component={Register}/>
             <Route path="/signin" component={SignIn}/>
+=======
+            <Route path="/signup" component={Form}/>
+>>>>>>> parent of 6b788a3 (Merge branch 'main' of https://github.com/LongTran0919/GymWeb into main)
+=======
+            <Route path="/signup" component={Form}/>
+>>>>>>> parent of 6b788a3 (Merge branch 'main' of https://github.com/LongTran0919/GymWeb into main)
         </Switch>
  </Router>
   );
