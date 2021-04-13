87acb3ddc3dcc76bd3afcb6a31d5c926490458b7
trangiaphuc <76146929+trangiaphuc@users.noreply.github.com>
2021-04-13 15:52:17 +0700

    set card

--- gymweb/src/App.js
+++ gymweb/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './Frontend/component/Navbar';
-import Header from './Frontend/component/Header';
 import Register from './Frontend/component/formRegister/Register';
 import SignIn from './Frontend/component/formSignIn/SignIn'
 import {BrowserRouter as Router,Switch,Route} from'react-router-dom';
+import Home from './Frontend/component/formHome/Home';
 
 
 
@@ -14,7 +14,7 @@ function App() {
     <Router>
         <Navbar/>
         <Switch>
-            <Route path="/home" component={Header}/>
+            <Route path="/home" component={Home}/>
             <Route path="/signup" component={Register}/>
             <Route path="/signin" component={SignIn}/>
         </Switch>
