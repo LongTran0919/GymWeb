c77dd8c35e83418b49d2560892f5e0afd8778ef1
LongTran0919 <hoanglong090320@gmail.com>
2021-04-14 14:09:34 +0700

    formTimKiem

--- gymweb/src/Frontend/component/Navbar.js
+++ gymweb/src/Frontend/component/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../IMG/soi.jpg"
+import logo from "../IMG/Soi.jpg"
 function Navbar(){
     
 
@@ -18,10 +18,10 @@ function Navbar(){
                     <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/about">About</a>
+                    <a className="nav-link" href="/products">Products</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/products">Services</a>
+                    <a className="nav-link" href="/about">About</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="/contacts">Contacts</a>
--- gymweb/src/Frontend/component/product/mainContent.js
+++ gymweb/src/Frontend/component/product/mainContent.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import product_card from "./data/product-data";
-import IMg from '../../IMG/unnamed.jpg';
+import IMg from '../../IMG/tay-removebg-preview.png';
 import '../../../App.css';
 
 const MainContent=()=>{
-    console.log(product_card);
+    const [nhomCo,setnhomCo]=useState("");
+    const [lv,setLV]=useState("");
     const listItems=product_card.map((item)=>
         <div className="card" key={item.id}>
             <div className="card_img">
@@ -20,6 +21,27 @@ const MainContent=()=>{
     );
     return(
         <div className="Container">
+            <div className="main_cbcontent">
+                <select className="custom-select cbselect" value={nhomCo} onChange={(e)=>{
+                    const selectNhomCo=e.target.value;
+                    setnhomCo(selectNhomCo);
+                }}>
+                    <option value="tay">Tay</option>
+                    <option value="nguc">Ngực</option>
+                    <option value="bung">Bụng</option>
+                    <option value="chan">Chân</option>
+                    <option value="mong">Mông</option>
+                </select>
+                <select className="custom-select cbselect" onChange={(e)=>{
+                    const selectLv=e.target.value;
+                    setLV(selectLv);
+                }}>
+                    <option value="lv1">1</option>
+                    <option value="lv2">2</option>
+                    <option value="lv3">3</option>
+                </select>
+                <button className="btnTim">Tìm kiếm</button>
+            </div>
             <div className="main_content">
                 {listItems}
             </div>
