63abeda0aa80d4f151649574f99df663867eae76
dajneem23 <56623206+dajneem23@users.noreply.github.com>
2021-05-22 13:09:20 +0700

    delete api
    delete api

--- GymWeb/src/Backend/Routes/ExcRouter.js
+++ GymWeb/src/Backend/Routes/ExcRouter.js
@@ -6,7 +6,7 @@ const passportConfig = require('../passport');
 const Exc= require('../Models/ExerciseModel');
 const cors = require('cors');
  
- ExcRouter.post('/add', (req,res)=>{
+ ExcRouter.post('/add', passport.authenticate('jwt',{session : false}),(req,res)=>{
 const {excName ,title,typeExc,level,taskList,date_created,comment,compound,author,calories,decription}=req.body;
         try{ const newExc = new Exc({ decription, excName ,title,typeExc,level,taskList,date_created,comment,compound,author,calories })
                 console.log(newExc)
@@ -41,6 +41,40 @@ const {excName ,title,typeExc,level,taskList,date_created,comment,compound,autho
                 return exc
             }, {}));
          });
+    })
+    ExcRouter.post('/delete',passport.authenticate('jwt',{session : false}),(req,res)=>{
+        const {id}=req.body
+        const{_id,username,role}=req.user;
+        if(role!='admin'){
+            res.status(500).json({
+            message:{msgBody:"you not have permision"},
+            msgError:true })
+           return;
+        }
+        Exc.findOneAndDelete({_id:id}, function(err, exc) {
+           
+
+            if(err){res.status(500).json({
+                message:{msgBody:"Error has occured 2"},
+                msgError:true })
+                return;
+            }
+            if(!exc)  { res.status(404).json({
+           
+                message:{msgBody:"not found Excercise  "},
+                msgError:false })
+                return;
+            }
+            if(exc) { res.status(200).json({
+           
+                message:{msgBody:"Excercise succesfully created"},
+                msgError:false })
+                return
+            }
+         
+            });
+       
+         
     })
 
 
--- gymweb/src/Backend/Service/ExerciseService.js
+++ gymweb/src/Backend/Service/ExerciseService.js
@@ -20,5 +20,14 @@ export default {
                 else return {"fail":"true"}
             })
         },
+        DeleteId:(id)=>{
+          return   axios
+          .post('http://localhost:5000/exercise/delete',id,{Headers: {
+                    'Content-Type': 'application/json',
+                  }}).then(res=>{
+            if(res.status!==401)return res.data;
+          else return {"fail":"true"}
+      })
+  },
 
 }
